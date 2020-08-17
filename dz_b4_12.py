a97763aca2e83f5a4c132c0e9eb28f1e7f031a00
Gadya <zakuson2001@inbox.ru>
2020-08-18 01:10:53 +0300

    final version

--- dz_b4_12.py
+++ dz_b4_12.py
@@ -1,6 +1,7 @@
 from users import UserRegister
 from find_athlete import DBMethods
 from datetime import datetime, date, time
+import os
 DB_PATH = "sqlite:///sochi_athletes.sqlite3"
 GENDER = ['male','female']
 
@@ -10,11 +11,6 @@ def isCorrectDate(enteredDate): # Функция проверки коррект
         return True
     except Exception:
         return False
-def birthdateToTimestamp(data):
-    ts=datetime.strptime(data, "%Y-%m-%d").timestamp()
-    return ts
-
-
 
 def isCorrectEmail(email):
     """
@@ -23,6 +19,12 @@ def isCorrectEmail(email):
     if email.find("@") >= 1 and email.count("@") == 1 and email.rfind(".") > email.find("@"):
         return True
     else:
+        return False
+def isCorrectHeight(height):
+    try:
+        int(height)
+        return True
+    except ValueError:
         return False
 
 def enterUserData():
@@ -41,25 +43,36 @@ def enterUserData():
             break
         else:
             print('Wrong date! Try one more time..')
-    height = input('Enter height (sm):')
+    while True:
+        height = input('Enter height (sm):')
+        if isCorrectHeight(height) and int(height) >= 100:
+            break
+        else:
+            print('Wrong height! It should be int number more then 100. Try one more time..')
     userData = [first_name,last_name,gender,email,birthdate,height]
     return userData 
 
 def main():
     
     while True:
+        print('=============================================================================')
         print('What do you want to do?\nType 1, 2, 3, 4 or 5\n1 - add new user\n2 - find most close user by user`s id to athlet from athlet`s db\n3 - show user`s db\n4 - delete record by id\n5 - quit')
         choose = input('-> ')
         if choose == '1':
+            os.system('cls||clear')
             newUserData = enterUserData() # Вводим данные нового пользователя
             regNewUser = UserRegister(DB_PATH, newUserData) # Регистрируем нового пользователя в БД
             regNewUser.addUserToDB()
             print('User {} {} registered..'.format(newUserData[0], newUserData[1]))
         elif choose == '2':
+            os.system('cls||clear')
             findRecord = DBMethods(DB_PATH)
             id = int(input('Enter id: '))
             if findRecord.isUserInDB(id):
-                findRecord.findCloseAthelets(id)
+                foundAthelets = findRecord.findCloseAthelets(id) # Получаем список с результатами поиска [id ближайшего по росту атлета, имя ближайшего по росту атлета, рост ближайшего по росту атлета, id ближайшего по дате рождения атлета, имя ближайшего по дате рождения атлета, дата рождения ближайшего по дате рождения атлета]
+                print('Most close athelet by height:\nid: {}\nname: {}\nheight: {}'.format(foundAthelets[0],foundAthelets[1],foundAthelets[2]))
+                print('=============================================================================')
+                print('Most close athelet by birthday:\nid: {}\nname: {}\nbirthday: {}'.format(foundAthelets[3],foundAthelets[4],foundAthelets[5]))
             else:
                 print('No Record with id {}..'.format(id))
         elif choose == '3':
@@ -68,6 +81,7 @@ def main():
             for item in records:
                 print('{}|{}|{}|{}|{}|{}|{}'.format(item.id,item.first_name,item.last_name,item.gender, item.email, item.birthdate, item.height))
         elif choose == '4':
+            os.system('cls||clear')
             delRecord = DBMethods(DB_PATH)
             id = int(input('Enter id: '))
             if delRecord.isUserInDB(id):
--- find_athlete.py
+++ find_athlete.py
@@ -1,8 +1,6 @@
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime, date, time
-import time
 Base = declarative_base()
 class UserDB(Base):
     __tablename__ = "user"
@@ -11,15 +9,13 @@ class UserDB(Base):
     last_name = sa.Column(sa.TEXT)
     gender = sa.Column(sa.TEXT)
     email = sa.Column(sa.TEXT)
-    # birthdate = sa.Column(sa.TEXT)
-    birthdate = sa.Column(sa.DATE)
+    birthdate = sa.Column(sa.TEXT)
     height = sa.Column(sa.REAL)
 class AtheleteDB(Base):
     __tablename__ = "athelete"
     id = sa.Column(sa.INTEGER, primary_key=True)
     age = sa.Column(sa.INTEGER)
-    # birthdate = sa.Column(sa.TEXT)
-    birthdate = sa.Column(sa.DATE)
+    birthdate = sa.Column(sa.TEXT)
     gender = sa.Column(sa.TEXT)
     height = sa.Column(sa.REAL)
     name = sa.Column(sa.TEXT)
@@ -32,13 +28,13 @@ class AtheleteDB(Base):
     country = sa.Column(sa.TEXT)
     
 class DBMethods():
-    def __init__(self, db_path):
+    def __init__(self, db_path): # В конструкторе создаём соединение с БД
         self.db_path = db_path
         self.engine = sa.create_engine(self.db_path)
         self.Sessions = sessionmaker(self.engine)
         self.session = self.Sessions()
 
-    def showUserDB(self):
+    def showUserDB(self): # Метод для отображения таблицы users зарегистрированных пользователей
         self.records = self.session.query(UserDB).all()
         return self.records
 
@@ -55,15 +51,16 @@ class DBMethods():
         self.session.query(UserDB).filter_by(id=self.id).delete()
         self.session.commit()
 
-    def findCloseAthelets(self, id):
+    def findCloseAthelets(self, id): 
+        # Метод, который на выходе выдаёт список с результатами поиска атлетов [id ближайшего по росту атлета, имя ближайшего по росту атлета, рост ближайшего по росту атлета, id ближайшего по дате рождения атлета, имя ближайшего по дате рождения атлета, дата рождения ближайшего по дате рождения атлета]
+        rezult = []
         self.id = id
         self.userRecord = self.session.query(UserDB).filter_by(id=self.id).first()
         self.userBirthday = self.userRecord.birthdate
         self.userHeight = self.userRecord.height
         # select * from athelete where height > 0 order by abs(150-height*100) limit 1;
         self.closeByHeightAthelete = self.session.query(AtheleteDB).filter(AtheleteDB.height > 0).order_by(sa.func.abs(AtheleteDB.height*100 - self.userHeight)).first()
-        self.closeByBirthdayAthelete = self.session.query(AtheleteDB).order_by(sa.func.abs(AtheleteDB.birthdate - self.userBirthday)).first()
-        print('{}:{}'.format(self.closeByBirthdayAthelete.id, self.closeByBirthdayAthelete.birthdate))
-
-
-        
+        # select * from athelete order by abs(julianday(birthdate) - julianday("1997-12-30")) limit 1;
+        self.closeByBirthdayAthelete = self.session.query(AtheleteDB).order_by(sa.func.abs(sa.func.julianday(AtheleteDB.birthdate) - sa.func.julianday(self.userBirthday))).first()
+        rezult = [self.closeByHeightAthelete.id, self.closeByHeightAthelete.name, self.closeByHeightAthelete.height, self.closeByBirthdayAthelete.id, self.closeByBirthdayAthelete.name, self.closeByBirthdayAthelete.birthdate]
+        return rezult 
